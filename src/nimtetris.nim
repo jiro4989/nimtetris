7bc1a4878e1dc773e0f371c95f747875781a9e4c
jiro4989 <jiroron666@gmail.com>
2019-02-17 03:39:32 +0900

    :sparkles:とりあえずHTMLからアクセスできるようにした

--- src/nimtetris.nim
+++ src/nimtetris.nim
@@ -35,10 +35,10 @@ const initialBoard: Board = @[
 
 var
   ## 現在のボード
-  currentBoard = initialBoard
+  currentBoard {.exportc.} = initialBoard
   ## 表示上のボード
   ## currentBoardに移動中のミノをセットしたりする
-  displayBoard = initialBoard
+  displayBoard {.exportc.} = initialBoard
 
 const minos = @[
   @[
@@ -81,7 +81,7 @@ const
   EMPTY_MINO = 0
   WALL_MINO = 1
 
-proc fetchBlock(b: Board, x, y: int): Block =
+proc fetchBlock(b: Board, x, y: int): Block {.exportc.} =
   ## x, y座標位置の4x4のミノブロックを取得する
   var i: int
   for y2 in y..<(y+4):
@@ -92,7 +92,7 @@ proc fetchBlock(b: Board, x, y: int): Block =
       inc j
     inc i
 
-proc isOverlap(self: Block, target: Block): bool =
+proc isOverlap(self: Block, target: Block): bool {.exportc.} =
   ## 対象のミノブロック同士に重なるものがあるかを判定する。
   ## 重なっていたらtrueを返す。
   for y, row in self:
@@ -103,56 +103,56 @@ proc isOverlap(self: Block, target: Block): bool =
         return true
   return false
 
-proc getBlock(m: Mino): Block =
+proc getBlock(m: Mino): Block {.exportc.} =
   return minos[m.minoIndex][m.rotateIndex]
 
-proc canMoveRight(m: Mino, b: Board): bool = 
+proc canMoveRight(m: Mino, b: Board): bool {.exportc.} = 
   if b[0].len < m.x + 1 + MINO_BLOCK_WIDTH:
     return false
   let blk = b.fetchBlock(x = m.x + 1, y = m.y)
   return not m.getBlock.isOverlap(blk)
 
-proc canMoveLeft(m: Mino, b: Board): bool = 
+proc canMoveLeft(m: Mino, b: Board): bool {.exportc.} = 
   if m.x - 1 < 0:
     return false
   let blk = b.fetchBlock(x = m.x - 1, y = m.y)
   return not m.getBlock.isOverlap(blk)
 
-proc canMoveDown(m: Mino, b: Board): bool = 
+proc canMoveDown(m: Mino, b: Board): bool {.exportc.} = 
   if b.len < m.y + 1 + MINO_BLOCK_WIDTH:
     return false
   let blk = b.fetchBlock(x = m.x, y = m.y + 1)
   return not m.getBlock.isOverlap(blk)
 
-proc moveRight(m: Mino) = m.x.inc
-proc moveLeft(m: Mino) = m.x.dec
-proc moveDown(m: Mino) = m.y.inc
+proc moveRight(m: Mino) {.exportc.} = m.x.inc
+proc moveLeft(m: Mino) {.exportc.} = m.x.dec
+proc moveDown(m: Mino) {.exportc.} = m.y.inc
 
-proc rotateRight(m: var Mino) =
+proc rotateRight(m: var Mino) {.exportc.} =
   let incedIndex = m.rotateIndex + 1
   m.rotateIndex = if minos[m.minoIndex].len <= incedIndex:
     0
   else:
     incedIndex
 
-proc rotateLeft(m: var Mino) =
+proc rotateLeft(m: var Mino) {.exportc.} =
   let decedIndex = m.rotateIndex - 1
   m.rotateIndex = if decedIndex < 0:
     minos[m.minoIndex].len - 1
   else:
     decedIndex
 
-proc isDeletable(row: seq[int]): bool =
+proc isDeletable(row: seq[int]): bool {.exportc.} =
   for c in row:
     if c <= 0:
       return false
   return true
 
-proc fetchRow(mb: MinoBoard, n: int): seq[int] =
+proc fetchRow(mb: MinoBoard, n: int): seq[int] {.exportc.} =
   let row = mb.board[n]
   return row[mb.offset .. row.len - 1 - mb.offset]
 
-proc setMino(b: var Board, m: Mino) =
+proc setMino(b: var Board, m: Mino) {.exportc.} =
   let blk = m.getBlock
   for y, row in blk:
     for x, cell in row:
@@ -160,17 +160,17 @@ proc setMino(b: var Board, m: Mino) =
       if cell != EMPTY_MINO:
         b[y+m.y][x+m.x] = cell
 
-proc updateDisplayBoard(m: Mino) =
+proc updateDisplayBoard(m: Mino) {.exportc.} =
   ## 表示用ボードに降下中のミノをセットする
   displayBoard = currentBoard
   displayBoard.setMino m
 
-proc updateCurrentBoard(m: Mino) =
+proc updateCurrentBoard(m: Mino) {.exportc.} =
   ## 現在のボードに降下不可能になったミノをセットする
   currentBoard.setMino m
   displayBoard = currentBoard
 
-proc newRandomMino(): Mino =
+proc newRandomMino(): Mino {.exportc.} =
   let r = random(max = minos.len)
   return Mino(minoIndex: r, x: 4, y: 0)
 
