e0108b587a7bd7f7f511bccde67391c414524549
jiro4989 <jiroron666@gmail.com>
2019-02-16 23:52:38 +0900

    :green_heart:テストコード追加

--- tests/tnimtetris.nim
+++ tests/tnimtetris.nim
@@ -9,6 +9,7 @@ suite "fetchBlock":
     @[0, 0, 0, 0, 0],
     @[1, 0, 0, 0, 1],
   ]
+
   test "左上取得":
     check(board.fetchBlock(0, 0) == [
       [1, 0, 0, 0],
@@ -40,3 +41,51 @@ suite "fetchBlock":
       [0, 0, 0, 0],
       [1, 0, 0, 0],
     ])
+
+suite "isOverlap":
+  const mino = [
+    [0, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 1, 1, 0],
+    [0, 0, 0, 0],
+  ]
+
+  test "重複あり":
+    check(mino.isOverlap([
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0],
+    ]) == true)
+
+  test "重複あり":
+    check(mino.isOverlap([
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]) == true)
+
+  test "重複あり":
+    check(mino.isOverlap([
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]) == true)
+
+  test "重複なし":
+    check(mino.isOverlap([
+      [1, 1, 1, 1],
+      [0, 1, 1, 1],
+      [0, 0, 0, 1],
+      [1, 1, 1, 1],
+    ]) == false)
+
+  test "重複なし":
+    check(mino.isOverlap([
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]) == false)
