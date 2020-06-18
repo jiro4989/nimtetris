f1619e82f96bc95258e4788da405d208c986bb43
jiro4989 <jiroron666@gmail.com>
2020-06-18 14:17:34 +0000

    add test cases

---
+++ tests/test_mino.nim
@@ -0,0 +1,41 @@
+import unittest
+
+include nimtetrispkg/mino
+
+suite "proc newRandomMino":
+  test "test":
+    let m = newRandomMino()
+    check m.x == 4
+    check m.y == 0
+
+suite "proc getBlock":
+  test "rotateIndex = 0, minoIndex = 0":
+    let m = Mino(rotateIndex: 0, minoIndex: 0)
+    let got = m.getBlock
+    let want = [
+        [EMPTY_MINO, EMPTY_MINO, EMPTY_MINO, EMPTY_MINO],
+        [EMPTY_MINO, FILLED_MINO2, FILLED_MINO2, EMPTY_MINO],
+        [FILLED_MINO2, FILLED_MINO2, EMPTY_MINO, EMPTY_MINO],
+        [EMPTY_MINO, EMPTY_MINO, EMPTY_MINO, EMPTY_MINO],
+      ]
+    check want == got
+  test "rotateIndex = 1, minoIndex = 0":
+    let m = Mino(rotateIndex: 1, minoIndex: 0)
+    let got = m.getBlock
+    let want = [
+        [EMPTY_MINO, FILLED_MINO2, EMPTY_MINO, EMPTY_MINO],
+        [EMPTY_MINO, FILLED_MINO2, FILLED_MINO2, EMPTY_MINO],
+        [EMPTY_MINO, EMPTY_MINO, FILLED_MINO2, EMPTY_MINO],
+        [EMPTY_MINO, EMPTY_MINO, EMPTY_MINO, EMPTY_MINO],
+      ]
+    check want == got
+  test "rotateIndex = 0, minoIndex = 1":
+    let m = Mino(rotateIndex: 0, minoIndex: 1)
+    let got = m.getBlock
+    let want = [
+        [EMPTY_MINO, EMPTY_MINO, EMPTY_MINO, EMPTY_MINO],
+        [EMPTY_MINO, FILLED_MINO3, FILLED_MINO3, EMPTY_MINO],
+        [EMPTY_MINO, EMPTY_MINO, FILLED_MINO3, FILLED_MINO3],
+        [EMPTY_MINO, EMPTY_MINO, EMPTY_MINO, EMPTY_MINO],
+      ]
+    check want == got
